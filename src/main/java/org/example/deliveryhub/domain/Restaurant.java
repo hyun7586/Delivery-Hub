bb0260bbcac8a65e45767a44c87bcc951465a7bc
Hyun <hyun_7586@naver.com>
2024-12-26 07:01:34 +0900

    refactor: 각 1:N 관계를 가지는 parent entity에 대해서 @OneToMany 작업
    - User, Restaurant, Order, Menu entity에 대해서 child entity List를 가지는 field 추가

--- src/main/java/org/example/deliveryhub/domain/Restaurant.java
+++ src/main/java/org/example/deliveryhub/domain/Restaurant.java
@@ -9,7 +9,9 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import java.time.LocalDateTime;
+import java.util.List;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -45,5 +47,11 @@ public class Restaurant {
 
   @Column(name="updated_at")
   private LocalDateTime updatedAt;
+
+  @OneToMany(mappedBy = "restaurant")
+  private List<Order> orderList;
+
+  @OneToMany(mappedBy = "restaurant")
+  private List<Menu> menuList;
 
 }
