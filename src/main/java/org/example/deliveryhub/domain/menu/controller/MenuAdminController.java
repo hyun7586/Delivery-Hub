cfef4b19418ae7faf1ee23851437e1aa81d008d9
Hyun <hyun_7586@naver.com>
2025-01-05 12:54:26 +0900

    feature: #5 implement menu CRUD api
    [구현 목록]
    - 단일 메뉴 정보 조회
    - 메뉴 리스트 조회
    - 특정 메뉴 정보 수정
    - 특정 메뉴 삭제
    
    - 단일 메뉴 정보 조회 for admin
    - 메뉴 리스트 조회 for admin
    
    close #5

---
+++ src/main/java/org/example/deliveryhub/domain/menu/controller/MenuAdminController.java
@@ -0,0 +1,41 @@
+package org.example.deliveryhub.domain.menu.controller;
+
+import java.util.List;
+import java.util.Optional;
+import lombok.RequiredArgsConstructor;
+import org.example.deliveryhub.domain.menu.entity.Menu;
+import org.example.deliveryhub.domain.menu.repository.MenuRepository;
+import org.example.deliveryhub.domain.menu.service.MenuService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/auth/menu")
+public class MenuAdminController {
+
+  private final MenuService menuService;
+  private final MenuRepository menuRepository;
+
+  // 특정 메뉴 조회 for admin
+  @GetMapping("/{menu_id}")
+  public ResponseEntity<Optional<Menu>> adminGetMenu(
+      @PathVariable(name="menu_id") Long menuId
+  ){
+    return ResponseEntity.status(HttpStatus.OK)
+        .body(menuRepository.findById(menuId));
+  }
+
+  // 메뉴 리스트 조회 for admin
+  @GetMapping("/all")
+  public ResponseEntity<List<Menu>> adminGetMenuList(){
+    return ResponseEntity.status(HttpStatus.OK)
+        .body(menuRepository.findAll());
+  }
+
+
+}
--- src/main/java/org/example/deliveryhub/domain/menu/service/MenuService.java
+++ src/main/java/org/example/deliveryhub/domain/menu/service/MenuService.java
@@ -1,6 +1,9 @@
 package org.example.deliveryhub.domain.menu.service;
 
+import java.util.List;
 import lombok.RequiredArgsConstructor;
+import org.example.deliveryhub.domain.menu.dto.MenuRequest;
+import org.example.deliveryhub.domain.menu.dto.MenuResponse;
 import org.example.deliveryhub.domain.menu.entity.Menu;
 import org.example.deliveryhub.domain.menu.repository.MenuRepository;
 import org.springframework.stereotype.Service;
@@ -12,10 +15,62 @@ public class MenuService {
   private final MenuRepository menuRepository;
 
   // 메뉴 조회
-  Menu getMenuById(Long id){
-    return menuRepository.findById(id).orElse(null);
+  public MenuResponse findById(Long id){
+
+    Menu result = menuRepository.findById(id).orElse(null);
+
+    if(result==null){
+      // throw Exception으로 변경할 예정
+      return null;
+    }
+
+    return MenuResponse.builder()
+        .name(result.getName())
+        .description(result.getDescription())
+        .price(result.getPrice())
+        .build();
   }
 
-  // Restaurant별 Menu List 반환은 어디서 함?
-  // Restaurant에 List<Menu> field가 있으니 RestaurantService에 구현해야 하나?
+  // 메뉴 리스트 조회
+  public List<MenuResponse> findAll(){
+
+    return menuRepository.findAll().stream()
+        .map(each -> MenuResponse.builder()
+            .name(each.getName())
+            .description(each.getDescription())
+            .price(each.getPrice())
+            .build())
+        .toList();
+  }
+
+  // 특정 메뉴 정보 수정
+  public MenuResponse updateMenu(Long menuId, MenuRequest request){
+
+    Menu target = menuRepository.findById(menuId).orElse(null);
+
+    if(target==null){
+      return null;
+    }
+
+    // request 객체에서 null이 아닌 field에 대해서만 수정
+    if(request.getRestaurant()!=null) target.setRestaurant(request.getRestaurant());
+    if(request.getName()!=null) target.setName(request.getName());
+    if(request.getDescription()!=null)  target.setDescription(request.getDescription());
+    if(request.getPrice()!=null)  target.setPrice(request.getPrice());
+    if(request.getOrderItemList()!=null)  target.setOrderItemList(request.getOrderItemList());
+
+    Menu result = menuRepository.save(target);
+
+    return MenuResponse.builder()
+        .name(result.getName())
+        .description(result.getDescription())
+        .price(result.getPrice())
+        .build();
+  }
+
+  // 특정 메뉴 삭제
+  public void deleteMenu(Long menuId){
+    menuRepository.deleteById(menuId);
+  }
+
 }
