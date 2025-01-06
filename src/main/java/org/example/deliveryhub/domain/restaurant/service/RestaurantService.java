0208410ad8a29efed97482f50f6ca294507081bf
Hyun <hyun_7586@naver.com>
2025-01-06 23:12:20 +0900

    feature: 식당 메뉴 리스트 조회 기능
    - add findAll() in RestaurantService

--- src/main/java/org/example/deliveryhub/domain/restaurant/service/RestaurantService.java
+++ src/main/java/org/example/deliveryhub/domain/restaurant/service/RestaurantService.java
@@ -1,7 +1,11 @@
 package org.example.deliveryhub.domain.restaurant.service;
 
+import java.util.List;
 import java.util.Optional;
 import lombok.RequiredArgsConstructor;
+import org.example.deliveryhub.domain.menu.dto.MenuResponse;
+import org.example.deliveryhub.domain.menu.repository.MenuRepository;
+import org.example.deliveryhub.domain.menu.service.MenuService;
 import org.example.deliveryhub.domain.restaurant.dto.RestaurantResponse;
 import org.example.deliveryhub.domain.restaurant.entity.Restaurant;
 import org.example.deliveryhub.domain.restaurant.mapper.RestaurantMapper;
@@ -13,6 +17,7 @@ import org.springframework.stereotype.Service;
 public class RestaurantService {
 
   private final RestaurantRepository restaurantRepository;
+  private final MenuRepository menuRepository;
   private final RestaurantMapper restaurantMapper;
 
   // 식당 정보 조회
@@ -22,6 +27,22 @@ public class RestaurantService {
     if(restaurant==null)  return null;
 
     return restaurantMapper.toResponse(restaurant);
+  }
+
+  // 식당 메뉴 리스트 조회
+  public List<MenuResponse> findAll(Long restaurantId){
+    Restaurant restaurant = restaurantRepository.findById(restaurantId).orElse(null);
+
+    // 이후 throw exception으로 처리
+    if(restaurant==null)  return null;
+
+    return restaurant.getMenuList().stream()
+        .map(each -> MenuResponse.builder()
+            .name(each.getName())
+            .description(each.getDescription())
+            .price(each.getPrice())
+            .build())
+        .toList();
   }
 
 }
